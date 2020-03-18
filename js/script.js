95e21d14878e30161bb00e01ecf480b77471b136
Clorhexidinum <mkubekov@gmail.com>
2020-03-18 18:45:10 +0300

    Все готово

--- js/script.js
+++ js/script.js
@@ -7,51 +7,6 @@ var popupName = popup.querySelector("[name=name]");
 var popupEmail = popup.querySelector("[name=email]");
 var popupComment = popup.querySelector("[name=comment]")
 
-var signIn = document.querySelector(".singIn");
-var loginForm = document.querySelector(".sign_in--form");
-var login = loginForm.querySelector("[name=login]");
-var password = loginForm.querySelector("[name=password]");
-var storaage = localStorage.getItem("login");
-
-var isStorageSupport = true;
-var storage = **;
-
-try {
-  storage = localStorage.getItem("login");
-} caych (err) {
-  isStorageSupport = false;
-}
-
-signIn.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  loginForm.classList.toggle("popup-show");
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  } else {
-    login.focus();
-  }
-});
-
-loginForm.addEventListener("submit", function (evt) {
-  if (!login.value || !password.value) {
-    evt.preventDefault();
-    console.log("Заполните все поля");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", login.value);
-    }
-  }
-});
-
-window.addEventListener("keydown", function (evt) {
-  if(evt.keyCode === 27) {
-    if ( signIn.classList.contains("popup-show")) {
-      evt.preventDefault();
-      signIn.classList.remove("popup-show");
-    }
-  }
-});
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
