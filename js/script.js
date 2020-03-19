2e190f095a9e5ed047367674498d19e00d4338f5
Clorhexidinum <mkubekov@gmail.com>
2020-03-19 19:43:31 +0300

    Перед проверкой

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@ var popupName = popup.querySelector("[name=name]");
 var popupEmail = popup.querySelector("[name=email]");
 var popupComment = popup.querySelector("[name=comment]")
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("popupName");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   //do work
 });
@@ -14,17 +23,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("popup-show");
-  popupName.focus()
+  if (storage) {
+    popupName.value = storage;
+    password.focus();
+  } else {
+    popupName.focus();
+  }
 });
 
 popupClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("popup-show");
+  popup.classList.remove("popup-error");
 });
 
 popupCloseOv.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("popup-show");
+  popup.classList.remove("popup-error");
+});
+
+
+popupForm.addEventListener("submit", function (evt) {
+  if (!popupName.value || !popupEmail.value || !popupComment.value) {
+    evt.preventDefault();
+    popup.classList.remove("popup-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("popupName", login.value);
+    }
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -32,6 +62,7 @@ window.addEventListener("keydown", function (evt) {
     if ( popup.classList.contains("popup-show")) {
       evt.preventDefault();
       popup.classList.remove("popup-show");
+      popup.classList.remove("popup-error");
     }
   }
 });
